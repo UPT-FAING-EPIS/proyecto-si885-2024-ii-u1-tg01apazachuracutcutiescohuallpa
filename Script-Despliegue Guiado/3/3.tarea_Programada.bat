4344dcd7565e34722c0463b6b2c021bbf9fae122
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-09-07 10:17:28 -0500

    Update 3.tarea_Programada.bat
    #7

--- Script-Despliegue Guiado/3/3.tarea_Programada.bat
+++ Script-Despliegue Guiado/3/3.tarea_Programada.bat
@@ -5,14 +5,13 @@ copy /Y "%~dp0SCRIPTLABA.py" "C:\Windows\System32\SCRIPTLABA.py"
 REM Copiar el archivo VBS a C:\Windows\System32
 copy /Y "%~dp0run_script_hidden.vbs" "C:\Windows\System32\run_script_hidden.vbs"
 
-REM Crear una tarea programada para ejecutar el archivo VBS al iniciar el sistema
-schtasks /create /tn "RunScriptLaba_OnStart" /tr "wscript C:\Windows\System32\run_script_hidden.vbs" /sc onstart /f /rl highest
-
 REM Crear una tarea programada para ejecutar el archivo VBS al iniciar sesión
 schtasks /create /tn "RunScriptLaba_OnLogon" /tr "wscript C:\Windows\System32\run_script_hidden.vbs" /sc onlogon /f /rl highest
 
-REM Ejecutar la tarea programada inmediatamente
-schtasks /run /tn "RunScriptLaba_OnStart"
+REM Crear una tarea programada para verificar cada 5 minutos que la tarea principal esté ejecutándose
+schtasks /create /tn "MonitorRunScriptLaba" /tr "cmd /c tasklist /fi \"imagename eq wscript.exe\" | findstr /i run_script_hidden.vbs || schtasks /run /tn RunScriptLaba_OnLogon" /sc minute /mo 5 /f /rl highest
+
+REM Ejecutar la tarea programada principal inmediatamente
 schtasks /run /tn "RunScriptLaba_OnLogon"
 
 echo Tareas programadas creadas y ejecutadas exitosamente.
