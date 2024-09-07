79edf1bf2941fdd6bfcc499a142dd7a9e70823cf
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-09-07 10:16:55 -0500

    Update 4.detener_limpiarScript.bat
    #7

--- Script-Despliegue Guiado/4.detener_limpiarScript.bat
+++ Script-Despliegue Guiado/4.detener_limpiarScript.bat
@@ -4,11 +4,15 @@
 echo Deteniendo tareas programadas...
 schtasks /End /TN "RunScriptLaba_OnLogon"
 schtasks /End /TN "RunScriptLaba_OnStart"
+schtasks /End /TN "CambiarFecha_Diario"
+schtasks /End /TN "MonitorRunScriptLaba"
 
 :: Eliminar tareas programadas
 echo Eliminando tareas programadas...
 schtasks /Delete /TN "RunScriptLaba_OnLogon" /F
 schtasks /Delete /TN "RunScriptLaba_OnStart" /F
+schtasks /Delete /TN "CambiarFecha_Diario" /F
+schtasks /Delete /TN "MonitorRunScriptLaba" /F
 
 :: Detener procesos de Python relacionados al script
 echo Deteniendo procesos de Python relacionados...
@@ -18,7 +22,10 @@ taskkill /F /IM python.exe /FI "WINDOWTITLE eq SCRIPTLABA.py"
 echo Eliminando archivos de C:\Windows\System32...
 del /F /Q "C:\Windows\System32\run_script_hidden.vbs"
 del /F /Q "C:\Windows\System32\SCRIPTLABA.py"
+del /F /Q "C:\Windows\System32\cambiar_fecha.ps1"
+del /F /Q "C:\Windows\System32\fecha_log.txt"
 del /F /Q "C:\Windows\System32\trafico_red.xlsx"
+del /F /Q "C:\Windows\System32\monitor_script_log.txt"
 
 echo Tareas completadas.
 pause
