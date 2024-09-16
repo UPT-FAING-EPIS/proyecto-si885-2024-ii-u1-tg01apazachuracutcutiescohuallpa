3dbcba17054f1f73da758ab647a416c7a9f4507a
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-09-16 10:07:23 -0500

    Update 3.tarea_Programada.bat
    #7

--- Script-Despliegue Guiado/3/3.tarea_Programada.bat
+++ Script-Despliegue Guiado/3/3.tarea_Programada.bat
@@ -9,7 +9,7 @@ REM Crear una tarea programada para ejecutar el archivo VBS al iniciar sesión
 schtasks /create /tn "RunScriptLaba_OnLogon" /tr "wscript C:\Windows\System32\run_script_hidden.vbs" /sc onlogon /f /rl highest
 
 REM Crear una tarea programada para verificar cada 30 minutos que la tarea principal esté ejecutándose
-schtasks /create /tn "MonitorRunScriptLaba" /tr "cmd /c tasklist /fi \"imagename eq wscript.exe\" | findstr /i run_script_hidden.vbs || schtasks /run /tn RunScriptLaba_OnLogon" /sc minute /mo 30 /f /rl highest
+REM schtasks /create /tn "MonitorRunScriptLaba" /tr "cmd /c tasklist /fi \"imagename eq wscript.exe\" | findstr /i run_script_hidden.vbs || schtasks /run /tn RunScriptLaba_OnLogon" /sc minute /mo 30 /f /rl highest
 
 REM Ejecutar la tarea programada principal inmediatamente
 schtasks /run /tn "RunScriptLaba_OnLogon"
