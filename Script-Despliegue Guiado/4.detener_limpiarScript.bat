8f017202ea3b11f380ccbed8d149bab9b015eb2e
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-10-16 11:20:53 -0500

    Update 4.detener_limpiarScript.bat
    #7

--- Script-Despliegue Guiado/4.detener_limpiarScript.bat
+++ Script-Despliegue Guiado/4.detener_limpiarScript.bat
@@ -15,8 +15,10 @@ taskkill /F /IM python.exe
 :: Eliminar archivos específicos en C:\Windows\System32
 echo Eliminando archivos de C:\Windows\System32...
 del /F /Q "C:\Windows\System32\run_script_hidden.vbs"
-del /F /Q "C:\Windows\System32\SCRIPTLABA.py"
+del /F /Q "C:\Windows\System32\SCRIPTLAB*.py"
 del /F /Q "C:\Windows\System32\last_date.txt"
+del /F /Q "C:\Windows\System32\aula.txt"
+del /F /Q "C:\Windows\System32\ip.txt"
 
 echo Tareas completadas.
 pause
